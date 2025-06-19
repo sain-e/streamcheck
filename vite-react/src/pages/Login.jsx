81ce5c0854f69452570428cd71ffcb7a4a272a21
sain-e <lyusaine@gmail.com>
2025-06-19 20:47:39 +0200

    Formulario login inicial

--- vite-react/src/pages/Login.jsx
+++ vite-react/src/pages/Login.jsx
@@ -1,4 +1,23 @@
+import { Box } from "@mui/material"
+import { useState } from "react";
+
 export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+
+    };
   
-    return (<h1>Login</h1>);
+    return (
+    <Box component="form" onSubmit={handleLogin}>
+        <TextField id="username-input" label="Usuario" variant="outlined" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <TextField type="password" id="password-input" label="Contraseña" variant="outlined" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <Button variant="contained" color="primary" type="submit">
+        Entrar
+        </Button>
+    </Box>
+    );
 }
